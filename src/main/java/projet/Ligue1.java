330c95dc7c49c4920c4e49817b365b7b8a14c7fd
SangminSHIM <tkdals4723@gmail.com>
2022-12-01 11:39:55 +0100

    Enum Club done

---
+++ src/main/java/projet/Ligue1.java
@@ -0,0 +1,14 @@
+package projet;
+
+public class Ligue1 {
+	Club club;
+	int ranking;
+	String season;
+	int match_played;
+	int win;
+	int lose;
+	int draw;
+	int points;
+	
+	
+}
--- src/main/java/projet/Main.java
+++ src/main/java/projet/Main.java
@@ -2,6 +2,8 @@ package projet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    	Ligue1 ligue = new Ligue1();
+    	
+        System.out.println(ligue.PSG);
     }
 }
