430e9bc10528967aa135ef1ee57910e375747d3b
HugoFerry <hugoferry0103@gmail.com>
2022-12-16 19:28:07 +0100

    Implémentation des Hooks

--- .githooks/Java/CommitMsgHook.java
+++ .githooks/Java/CommitMsgHook.java
@@ -1,6 +1,23 @@
-public class CommitMsgHook {
-    public static void main(String[] args) {
+import java.io.*;
 
-        System.out.println(args);
+public class CommitMsgHook {
+    public static void main (String[] args) throws IOException{
+
+        File file = new File(args[0]);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        st = br.readLine();
+        System.out.println("length = "+st.length());
+        while (st != null){
+            System.out.println("Commit message :");
+            System.out.println("\""+st+"\"");
+            boolean res = (st.length()%2 == 0);
+            System.out.println("Nombre de caractère : "+res);
+
+            st = br.readLine();
+        }
+        System.exit(0);
     }
 }
--- .githooks/Java/PrePushHook.java
+++ .githooks/Java/PrePushHook.java
@@ -1,7 +1,21 @@
+import java.io.*;
+
 public class PrePushHook {
-    public static void main(String[] args) {
-        for(int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+    public static void main (String[] args) throws IOException{
+
+        // Args[0] == local
+        // Args[1] == remote
+
+        System.out.println("Local :"+args[0]);
+        System.out.println("Remote :"+args[1]);
+
+        boolean res = (args[1].contains("HugoFerry"));
+        System.out.println("Bonjour, 'HugoFerry' dans le remote, push accepté");
+        System.out.println(res);
+        if(res){
+            System.exit(0);
+        }else {
+            System.exit(1);
         }
     }
 }
